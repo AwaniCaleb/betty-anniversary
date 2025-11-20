79ecfd696c5a871290f691ae96806e409da16812
Awani Caleb <calebawani@gmail.com>
2025-11-20 13:56:28 +0100

    chore: initialize Vite project with basic configuration
    - Added package.json with Vite as a dependency and defined scripts for development, build, and preview.
    - Created vite.config.js to set the output directory for builds to 'dist'.

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    // Vite automatically sets the output directory to 'dist'
+    build: {
+        outDir: 'dist'
+    }
+})
